56827702bd250ff2a18a2db375253d194687a212
Elias Re <eliasrenawi23@gmail.com>
2022-06-19 13:56:35 +0300

    add project

---
+++ EliasWebApp/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using EliasWebApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace EliasWebApp.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+         public BookController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+
+  
+    }
+}
